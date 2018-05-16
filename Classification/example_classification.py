aac5498ec446cfe4a675698d8850c2e6e71d4bcd
antonstenaxel <antkarls@student.chalmers.se>
2018-05-16 12:55:52 +0200

    Updated with some comments

--- Classification/example_classification.py
+++ Classification/example_classification.py
@@ -1,11 +1,13 @@
 from baseline_cnn_classifier import Classifier
 import sys
 
-cf = Classifier("Models/baseline_cnn.h5")
+cf = Classifier(path_to_model = "Models/baseline_cnn.h5")
 
 if(len(sys.argv) > 1):
     image_path = sys.argv[1]
 else:
     print("Provide filepath to image as argument")
 
-cf.predict(image_path)
+# Returns a 27 length array with probabilities correspongding to cf.dic
+# Accepts both images as file paths and numpy matrices
+cf.predict(img = image_path, print_result=True)
